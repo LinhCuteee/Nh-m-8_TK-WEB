8f3037ccf34f5bbb1f8de323001664591fe4f007
LinhCuteee <152121455+LinhCuteee@users.noreply.github.com>
2023-12-31 16:17:49 +0700

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,25 +1,28 @@
-// validation form register and register user local storage
 const inputUsernameRegister = document.querySelector(".input-signup-username");
 const inputPasswordRegister = document.querySelector(".input-signup-password");
+const inputRepeatPassword = document.querySelector(".input-signup-repeatpassword");
 const btnRegister = document.querySelector(".signup__signInButton");
-
-// validation form register and register user local storage
 
 btnRegister.addEventListener("click", (e) => {
   e.preventDefault();
-  if (
-    inputUsernameRegister.value === "" ||
-    inputPasswordRegister.value === ""
-  ) {
-    alert("vui lòng không để trống");
+
+  const existingUser = localStorage.getItem(inputUsernameRegister.value);
+
+  if (inputUsernameRegister.value === "" || inputPasswordRegister.value === "" || inputRepeatPassword.value === "") {
+    alert("Vui lòng không để trống");
+  } else if (existingUser) {
+    alert("Email đã đăng ký. Vui lòng chọn email khác.");
+  } else if (inputPasswordRegister.value !== inputRepeatPassword.value) {
+    alert("Mật khẩu không khớp. Vui lòng nhập lại mật khẩu.");
   } else {
-    // array user
     const user = {
       username: inputUsernameRegister.value,
       password: inputPasswordRegister.value,
     };
+
     let json = JSON.stringify(user);
     localStorage.setItem(inputUsernameRegister.value, json);
+
     alert("Đăng Ký Thành Công");
     window.location.href = "login.html";
   }
